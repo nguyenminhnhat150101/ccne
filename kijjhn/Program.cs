b890e6969bf3b1c1f2357ed3850dffeed3d27c35
Nguyễn Nhật <nguyenminhnhat15012001@gmail.com>
2020-12-01 19:41:27 +0700

    Add project files.

---
+++ kijjhn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kijjhn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
